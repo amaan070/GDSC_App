4f7df42ba6af231a009665bb1ea5a40367213301
amaan070 <pramn070@gmail.com>
2024-03-31 16:50:51 +0530

    SMALL Change

--- lib/src/pages/chatbot/chat_bot.dart
+++ lib/src/pages/chatbot/chat_bot.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:provider/provider.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
